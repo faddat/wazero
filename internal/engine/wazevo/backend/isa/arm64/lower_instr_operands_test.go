f5c90410729b6b8d24b0845ad56fafea0fde964b
Takeshi Yoneda <t.y.mathetake@gmail.com>
2023-06-30 14:14:06 +1000

    more
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/engine/wazevo/backend/isa/arm64/lower_instr_operands_test.go
+++ internal/engine/wazevo/backend/isa/arm64/lower_instr_operands_test.go
@@ -109,6 +109,38 @@ func TestMachine_getOperand_NR(t *testing.T) {
 }
 
 func TestMachine_getOperand_SR_NR(t *testing.T) {
+	ishlWithConstAmount := func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
+		blk := builder.CurrentBlock()
+		// (p1+p2) << p3
+		p1 := blk.AddParam(builder, ssa.TypeI64)
+		p2 := blk.AddParam(builder, ssa.TypeI64)
+		add := builder.AllocateInstruction()
+		add.AsIadd(p1, p2)
+		builder.InsertInstruction(add)
+		addResult := add.Return()
+
+		amount := builder.AllocateInstruction()
+		amount.AsIconst32(14)
+		builder.InsertInstruction(amount)
+
+		amountVal := amount.Return()
+
+		ishl := builder.AllocateInstruction()
+		ishl.AsIshl(addResult, amountVal)
+		builder.InsertInstruction(ishl)
+
+		ctx.definitions[p1] = &backend.SSAValueDefinition{BlkParamVReg: backend.VReg(1), BlockParamValue: p1}
+		ctx.definitions[p2] = &backend.SSAValueDefinition{BlkParamVReg: backend.VReg(2), BlockParamValue: p2}
+		ctx.definitions[addResult] = &backend.SSAValueDefinition{Instr: add, N: 0}
+		ctx.definitions[amountVal] = &backend.SSAValueDefinition{Instr: amount, N: 0}
+
+		ctx.vRegMap[addResult] = backend.VReg(1234)
+		ctx.vRegMap[ishl.Return()] = backend.VReg(10)
+		def = &backend.SSAValueDefinition{Instr: ishl, N: 0}
+		mode = extModeNone
+		return
+	}
+
 	for _, tc := range []struct {
 		name         string
 		setup        func(*mockCompilationContext, ssa.Builder, *machine) (def *backend.SSAValueDefinition, mode extMode)
@@ -121,7 +153,7 @@ func TestMachine_getOperand_SR_NR(t *testing.T) {
 				blk := builder.CurrentBlock()
 				v := blk.AddParam(builder, ssa.TypeI64)
 				def = &backend.SSAValueDefinition{BlkParamVReg: regToVReg(x4), BlockParamValue: v}
-				return def, extModeZeroExtend64
+				return def, extModeNone
 			},
 			exp: operandNR(regToVReg(x4)),
 		},
@@ -149,48 +181,88 @@ func TestMachine_getOperand_SR_NR(t *testing.T) {
 
 				ctx.vRegMap[ishl.Return()] = backend.VReg(10)
 				def = &backend.SSAValueDefinition{Instr: ishl, N: 0}
-				return def, extModeZeroExtend64
+				return def, extModeNone
 			},
 			exp: operandNR(backend.VReg(10)),
 		},
 		{
-			name: "ishl with const amount",
+			name:  "ishl with const amount",
+			setup: ishlWithConstAmount,
+			exp:   operandSR(backend.VReg(1234), 14, shiftOpLSL),
+		},
+		{
+			name: "ishl with const amount but group id is different",
 			setup: func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
-				blk := builder.CurrentBlock()
-				// (p1+p2) << p3
-				p1 := blk.AddParam(builder, ssa.TypeI64)
-				p2 := blk.AddParam(builder, ssa.TypeI64)
-				add := builder.AllocateInstruction()
-				add.AsIadd(p1, p2)
-				builder.InsertInstruction(add)
-				addResult := add.Return()
-
-				amount := builder.AllocateInstruction()
-				amount.AsIconst32(14)
-				builder.InsertInstruction(amount)
-
-				amountVal := amount.Return()
-
-				ishl := builder.AllocateInstruction()
-				ishl.AsIshl(addResult, amountVal)
-				builder.InsertInstruction(ishl)
-
-				ctx.definitions[p1] = &backend.SSAValueDefinition{BlkParamVReg: backend.VReg(1), BlockParamValue: p1}
-				ctx.definitions[p2] = &backend.SSAValueDefinition{BlkParamVReg: backend.VReg(2), BlockParamValue: p2}
-				ctx.definitions[addResult] = &backend.SSAValueDefinition{Instr: add, N: 0}
-				ctx.definitions[amountVal] = &backend.SSAValueDefinition{Instr: amount, N: 0}
-
-				ctx.vRegMap[addResult] = backend.VReg(1234)
-				def = &backend.SSAValueDefinition{Instr: ishl, N: 0}
-				return def, extModeZeroExtend64
+				def, mode = ishlWithConstAmount(ctx, builder, m)
+				m.currentGID = 1230
+				return
 			},
-			exp: operandSR(backend.VReg(1234), 14, shiftOpLSL),
+			exp: operandNR(backend.VReg(10)),
+		},
+		{
+			name: "ishl with const amount but ref coount is larger than 1",
+			setup: func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
+				def, mode = ishlWithConstAmount(ctx, builder, m)
+				def.RefCount = 10
+				return
+			},
+			exp: operandNR(backend.VReg(10)),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			ctx, b, m := newSetupWithMockContext()
 			def, mode := tc.setup(ctx, b, m)
 			actual := m.getOperand_SR_NR(def, mode)
+			require.Equal(t, tc.exp, actual)
+			require.Equal(t, strings.Join(tc.instructions, "\n"), formatEmittedInstructions(m))
+		})
+	}
+}
+
+func TestMachine_getOperand_Imm12_ER_SR_NR(t *testing.T) {
+	for _, tc := range []struct {
+		name         string
+		setup        func(*mockCompilationContext, ssa.Builder, *machine) (def *backend.SSAValueDefinition, mode extMode)
+		exp          operand
+		instructions []string
+	}{
+		{
+			name: "block param",
+			setup: func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
+				blk := builder.CurrentBlock()
+				v := blk.AddParam(builder, ssa.TypeI64)
+				def = &backend.SSAValueDefinition{BlkParamVReg: regToVReg(x4), BlockParamValue: v}
+				return def, extModeZeroExtend64
+			},
+			exp: operandNR(regToVReg(x4)),
+		},
+		{
+			name: "const imm 12 no shift",
+			setup: func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
+				cinst := builder.AllocateInstruction()
+				cinst.AsIconst32(0xfff)
+				builder.InsertInstruction(cinst)
+				def = &backend.SSAValueDefinition{Instr: cinst, N: 0}
+				return
+			},
+			exp: operandImm12(0xfff, 0),
+		},
+		{
+			name: "const imm 12 with shift bit",
+			setup: func(ctx *mockCompilationContext, builder ssa.Builder, m *machine) (def *backend.SSAValueDefinition, mode extMode) {
+				cinst := builder.AllocateInstruction()
+				cinst.AsIconst32(0xabc_000)
+				builder.InsertInstruction(cinst)
+				def = &backend.SSAValueDefinition{Instr: cinst, N: 0}
+				return
+			},
+			exp: operandImm12(0xabc, 1),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx, b, m := newSetupWithMockContext()
+			def, mode := tc.setup(ctx, b, m)
+			actual := m.getOperand_Imm12_ER_SR_NR(def, mode)
 			require.Equal(t, tc.exp, actual)
 			require.Equal(t, strings.Join(tc.instructions, "\n"), formatEmittedInstructions(m))
 		})
